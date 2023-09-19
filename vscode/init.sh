35f2983896ed36115f99a3eb004801e7035e0f4a
micimize <rosenthalm93@gmail.com>
2023-09-18 18:26:05 -0700

    nullglob

--- vscode/init.sh
+++ vscode/init.sh
@@ -1,6 +1,8 @@
 if [ -n "${VSCODE_SESSION}" ]; then
+  shopt -s nullglob
   for activation_file in .vscode/*activate.sh; do
     source $activation_file
   done
+  shopt -u nullglob
   # medium_prompt
 fi

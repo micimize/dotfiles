e6f60cc6145a42295e0c98fe5c6e4ad88c8755cb
micimize <rosenthalm93@gmail.com>
2023-11-01 09:29:44 -0700

    grab blackbox settings

--- vscode/init.sh
+++ vscode/init.sh
@@ -1,27 +1,27 @@
 # current_tmux_session defined in bash/utils.sh
 
 function vscode_session_prefix {
-  session="${1:-$(current_tmux_session)}"
+  session="${1:-`current_tmux_session`}"
   prefix="${session%%/*}"
   echo "$prefix"
 }
 
 function vscode_session_number {
-  session="${1:-$(current_tmux_session)}"
+  session="${1:-`current_tmux_session`}"
   name="${session##*/}"
   number="${name%%_*}"
   echo "$number"
 }
 
 function vscode_session_label {
-  session="${1:-$(current_tmux_session)}"
+  session="${1:-`current_tmux_session`}"
   name="${session##*/}"
   task="${name#*_}"
   echo "$task"
 }
 
 function vscode_pack_sessions {
-  session_prefix="${1:-$(vscode_session_prefix)}"
+  session_prefix="${1:-`vscode_session_prefix`}"
   num=0
   sessions=`tmux ls -F '#{session_name}' | grep "$session_prefix*" | sort`
   while read session; do

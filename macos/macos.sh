6a618f462e1d23d38bed2fc1e197c6ffbe4894aa
micimize <rosenthalm93@gmail.com>
2025-07-13 11:08:25 -0700

    some old changes

--- macos/macos.sh
+++ macos/macos.sh
@@ -12,8 +12,10 @@ export FIREFOX_PROFILE_DIR="$HOME/Library/Application Support/Firefox/Profiles/l
 export VSCODE_CONFIG_DIR="$HOME/.config/Code/User"
 export VSCODE_CONFIG_DIR="$HOME/Library/Application Support/Code/User"
 
-export NVM_DIR="/Users/mjr/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
 alias fixaudio="sudo killall coreaudiod"
 function fixbluetooth {

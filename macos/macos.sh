3130659bdf136d7ff3464f66aa65c55246c43e62
micimize <rosenthalm93@gmail.com>
2026-01-15 15:27:39 -0800

    stuff n such

--- macos/macos.sh
+++ macos/macos.sh
@@ -14,10 +14,6 @@ export VSCODE_CONFIG_DIR="$HOME/.config/Code/User"
 export VSCODE_CONFIG_DIR="$HOME/Library/Application Support/Code/User"
 
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
-
 alias fixaudio="sudo killall coreaudiod"
 function fixbluetooth {
   sudo kextunload -b com.apple.iokit.BroadcomBluetoothHostControllerUSBTransport
@@ -27,8 +23,6 @@ function fixbluetooth {
 # Add environment variable COCOS_CONSOLE_ROOT for cocos2d-x
 export COCOS_CONSOLE_ROOT=/Users/mjr/Documents/code/internal/cocos2d-js-v3.6/tools/cocos2d-console/bin
 export PATH=$COCOS_CONSOLE_ROOT:$PATH
-
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
 # TODO ?
 source /Users/mjr/Library/Preferences/org.dystroy.broot/launcher/bash/br
